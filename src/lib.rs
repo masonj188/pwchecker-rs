e2a5dbf1c97c54b22eb1d9e92358f7f131f73fbd
Mason Johnson <mason@invariant.dev>
2021-04-25 21:36:40 -0600

    Added better documentation and altered a test case

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+//! pwcehcker_rs
+//!
+//! pwchecker_rs allows you to conveniently query the [haveibeenpwned.com](https://haveibeenpwned.com)
+//! api so you can check whether or not a password has been involved in a data breach.
+//!
+//! # Examples
+//! ```
+//! let res = pwchecker_rs::check_for_pwnage("helloworld");
+//!
+//! assert!(res.unwrap().times_pwned > 0);
+//! ```
+
 use std::error::Error;
 
 use crypto::digest::Digest;
@@ -6,12 +18,21 @@ use reqwest::blocking;
 
 const API_URL: &str = "https://api.pwnedpasswords.com/range/";
 
+/// Passwd contains two fields, the password checked for pwnage and the number of times
+/// that password has been pwned.
 #[derive(Debug)]
 pub struct Passwd {
     pub text: String,
     pub times_pwned: i32,
 }
 
+/// Checks given password for a breach according to haveibeenpwned.com.
+///
+/// # Examples
+/// ```
+/// let res = pwchecker_rs::check_for_pwnage("helloworld");
+///
+/// assert!(res.unwrap().times_pwned > 0);
 pub fn check_for_pwnage(pass: &str) -> Result<Passwd, Box<dyn Error>> {
     if pass.len() <= 0 {
         return Err("Password can't be length 0")?;
@@ -48,18 +69,15 @@ fn get_hash(pass: &str) -> String {
 
 #[cfg(test)]
 mod tests {
-
+    use super::*;
     #[test]
     #[should_panic]
     fn test_zero_len() {
-        super::check_for_pwnage("").unwrap();
+        check_for_pwnage("").unwrap();
     }
 
     #[test]
     fn check_hello_world() {
-        assert_eq!(
-            super::check_for_pwnage("helloworld").unwrap().times_pwned,
-            16418
-        );
+        assert!(check_for_pwnage("helloworld").unwrap().times_pwned > 0);
     }
 }
